686c61880d7f68350ac9dfb14bc24a44ed81c937
sameer <sameershinde@gmail.com>
2023-04-26 17:24:53 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './component/Home';
+import About from './component/About'
+
+export default function App() {
+  return (
+    <div className="App">
+
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>}/>
+     </Routes>
+     </BrowserRouter>
+     
+    </div>
+  );
+}
---
+++ src/User1.js
@@ -0,0 +1,22 @@
+
+export function User1(){
+    return(
+        <>
+        <h1>hello user1</h1>
+        
+        </>
+    );
+}
+
+// function User2(){
+//     return(
+//         <>
+//         <h1>hello user2</h1>
+        
+//         </>
+//     );
+// }
+
+
+// export default User1;
+//  User2;
