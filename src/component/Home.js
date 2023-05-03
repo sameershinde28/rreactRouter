556a35b10da7b5705cd71db4ac3d947992c98976
sameer <sameershinde@gmail.com>
2023-05-03 15:01:53 +0530

    nav to another page using function

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,16 +2,19 @@ import {Link,useNavigate} from 'react-router-dom';
 
 export default function Home(){
    const navigate=useNavigate();
-   
+    const navToPage=(url)=>{
+        navigate(url);
+
+    }
    return <>
     <h1>home component</h1>
     <p>This is Home page of our awesome app</p>
     <p>we are learning react router here</p>
     <Link to="/about">Go to about page</Link>
     <br/> <br/>
-    <button onClick={()=>navigate('/about')}  >Go to about</button>
+    <button onClick={()=>navToPage('/about')}  >Go to about</button>
     <br/> <br/>
-    <button onClick={()=>navigate('/filter')}>Go to filter</button>
+    <button onClick={()=>navToPage('/filter')}>Go to filter</button>
     <br/> <br/>
     
 
