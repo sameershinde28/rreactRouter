1207c370e111e1862959ebfde1b84e164546a5a5
sameer <sameershinde@gmail.com>
2023-05-03 15:54:29 +0530

    useLocation

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,9 @@
-import {Link,useNavigate} from 'react-router-dom';
+import {Link,useNavigate,useLocation} from 'react-router-dom';
 
 export default function Home(){
+    const location=useLocation();
+    console.log(location);
+
    const navigate=useNavigate();
     const navToPage=(url)=>{
         navigate(url);
