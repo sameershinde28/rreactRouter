80b70642d589008999b1244044703b4aad1d48da
sameer <sameershinde@gmail.com>
2023-05-03 14:43:25 +0530

    input and button added

--- src/component/Filter.js
+++ src/component/Filter.js
@@ -18,6 +18,10 @@ export default function Filter(){
         <h3>my name is :: {name}</h3>
         <h3>my age is :: {age}</h3>
         <button onClick={()=>setSameer({age:50})}>click to add  age in query</button>
+        
+        <input type="text" onChange={(e)=>setSameer({text:e.target.value,age:10})}
+        placeholder="enter text in query"  />
+        
         </div>
 
     )
